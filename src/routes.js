3fc8d2f49133eedb4f4f84844d4ea03a4a06966b
gabrielcoutinhose <gabrielctnhse@gmail.com>
2025-01-18 20:15:05 -0300

    feat: add session controller; to perform login

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import UserController from "./app/controllers/User.controller";
+import SessionController from "./app/controllers/Session.controller";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+
+routes.post("/users/sessions", SessionController.store);
 
 export default routes;
