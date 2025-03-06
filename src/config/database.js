cc189ab578d66de501090e48a0a7ba27092bbb1a
gabrielcoutinhose <gabrielctnhse@gmail.com>
2025-03-06 19:53:20 -0300

    hotfix: on mongo config/database.js; removing the deprecated options, because it's standarnd now

--- src/config/database.js
+++ src/config/database.js
@@ -23,9 +23,6 @@ module.exports = {
     uri:
       process.env.MONGO_URI ||
       `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`,
-    mongooseOptions: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
+    mongooseOptions: {},
   },
 };
