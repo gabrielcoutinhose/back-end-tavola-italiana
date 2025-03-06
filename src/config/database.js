08542c9ea5b1dc13a1455acfd8bdc2a1287cff17
gabrielcoutinhose <gabrielctnhse@gmail.com>
2025-03-06 19:38:28 -0300

    feat: new indexer and dynamic credential passing using dynamic .env (in this test case)

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,31 @@
 require("dotenv").config();
 
 module.exports = {
-  username: process.env.POSTGRES_USER || "your_postgres_user",
-  password: process.env.POSTGRES_PASSWORD || "your_postgres_password",
-  database: process.env.POSTGRES_DB || "psqldb",
-  host: process.env.POSTGRES_HOST || "postgres",
-  port: process.env.POSTGRES_PORT || "5432",
-  dialect: "postgres",
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  postgres: {
+    username: process.env.POSTGRES_USER || "your_postgres_user",
+    password: process.env.POSTGRES_PASSWORD || "your_postgres_password",
+    database: process.env.POSTGRES_DB || "psqldb",
+    host: process.env.POSTGRES_HOST || "postgres",
+    port: process.env.POSTGRES_PORT || "5432",
+    dialect: "postgres",
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  mongo: {
+    username: process.env.MONGO_USER || "your_mongo_user",
+    password: process.env.MONGO_PASSWORD || "your_mongo_password",
+    database: process.env.MONGO_DB || "your_mongo_db",
+    host: process.env.MONGO_HOST || "mongo",
+    port: process.env.MONGO_PORT || "27017",
+    uri:
+      process.env.MONGO_URI ||
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`,
+    mongooseOptions: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
   },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,22 +1,45 @@
 import Sequelize from "sequelize";
+import { Mongoose } from "mongoose";
+
 import configDatabase from "../config/database";
 import User from "../app/models/User.model";
 import Product from "../app/models/Product.model";
 import Category from "../app/models/Category.model";
 
-const models = [User, Product, Category];
+const postgresModels = [User, Product, Category];
+const mongoModels = [];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
+
   init() {
-    this.connection = new Sequelize(configDatabase);
-    models
+    this.connection = new Sequelize(configDatabase.postgres);
+    postgresModels
       .map((model) => model.init(this.connection))
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo() {
+    this.mongoConnection = Mongoose.connect(
+      configDatabase.mongo.uri,
+      configDatabase.mongo.mongooseOptions,
+    )
+      .then(() => {
+        console.log("Connection to MongoDB established successfully!");
+        mongoModels.forEach((model) => {
+          if (model.mongoInit) {
+            model.mongoInit(Mongoose);
+          }
+        });
+      })
+      .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+      });
   }
 }
 
